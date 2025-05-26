e85b3ee31f08449c1a235993c7f0e994b960f6d7
Miguel Pereira <60988968+miguel617@users.noreply.github.com>
2025-05-26 13:24:38 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -128,7 +128,7 @@ const checkPortAvailable = (port: number): Promise<boolean> => {
 
 const startAgents = async () => {
   const directClient = new DirectClient();
-  let serverPort = parseInt(settings.SERVER_PORT || "3000");
+  let serverPort = parseInt(process.env.PORT || settings.SERVER_PORT || "3000");
   const args = parseArguments();
 
   let charactersArg = args.characters || args.character;
