436e353bdff6cab921fadbfb9e273a1fae89b651
Miguel Pereira <60988968+miguel617@users.noreply.github.com>
2025-05-26 14:24:22 +0100

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import { PostgresDatabaseAdapter } from "@elizaos/adapter-postgres";
 import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
-#import Database from "better-sqlite3";
+// import Database from "better-sqlite3";
 import path from "path";
 
 export function initializeDatabase(dataDir: string) {
